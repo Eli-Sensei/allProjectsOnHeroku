6a8f6701da6a4672588f3a9d98a6d4ac66030883
Ellyas-San <mepereiradebarros@edenschool.fr>
2020-10-23 12:01:09 +0200

    modification des liens

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+        
+        *{box-sizing: border-box;}
+        
+        body{
+           font-family: 'Nunito', sans-serif;
+           background-color: #343434;
+           color: seashell;
+           margin: 0;
+        }
+
+        div{
+            height: 50px;
+            width: 200px;
+
+            border: 1px solid black;
+            
+        }
+        a{
+            color: inherit;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    
+    
+    
+    <?php
 
 const HEROKU_DIRS = ['.', '..', '.composer', '.heroku', '.profile.d', 'vendor'];
- foreach(array_diff(scandir('.', SCANDIR_SORT_ASCENDING), HEROKU_DIRS) as $v):
-    if(is_dir($v)) echo '<br><a target="_blank" href="'.$v.'">'.$v.'</a>';
- endforeach;
+foreach(array_diff(scandir('.', SCANDIR_SORT_ASCENDING), HEROKU_DIRS) as $v):
+    if(is_dir($v)) echo '<div><a target="_blank" href="'.$v.'">'.$v.'</a></div>';
+endforeach;
+
+?>
+
+</body>
+</html>
