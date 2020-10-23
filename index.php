eea93fadf72d2d03b64f86d188e8ad81ee2c5713
Ellyas-San <mepereiradebarros@edenschool.fr>
2020-10-23 11:54:25 +0200

    pouet

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-phpinfo();
+const HEROKU_DIRS = ['.', '..', '.composer', '.heroku', '.profile.d', 'vendor'];
+ foreach(array_diff(scandir('.', SCANDIR_SORT_ASCENDING), HEROKU_DIRS) as $v):
+    if(is_dir($v)) echo '<br><a target="_blank" href="'.$v.'">'.$v.'</a>';
+ endforeach;
